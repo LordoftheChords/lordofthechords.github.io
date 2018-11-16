39ef595b42c89018452ad55b76163df7c373af6b
phangjunyu <phangjunyu@gmail.com>
2018-11-16 23:38:53 +0800

    added timer with no css

--- js/main.js
+++ js/main.js
@@ -33,6 +33,8 @@ $(document).ready(function() {
         $(this).find(".collapse-icon").text("+");
       }
     })
+
+    timer();
 });
 
 
@@ -58,3 +60,34 @@ $('.menu-link').on('click', function() {
     $('#main-menu').removeClass("show");
     $('#scroll-menu').removeClass("show");
 });
+
+function timer(){
+  // Set the date we're counting down to
+  var countDownDate = new Date("Feb 12, 2019 00:00:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("kickstarter-timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("kickstarter-timer").innerHTML = "WE ARE LIVE HERE: [link]";
+    }
+  }, 1000);
+}
